acec91ec67e237cd8068e3157f5b45d35b481740
tmtuan04 <tuminhtuana7@gmail.com>
2025-03-22 15:27:40 +0700

    Update folder and Config Backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import path from "path";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
@@ -8,28 +7,27 @@ import userRouter from "./routes/userRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
 import uploadRouter from "./routes/uploadRoutes.js";
 
+import { connectDB } from "./lib/db.js";
+
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("connected to db");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+// Connect Database
+connectDB();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/keys/paypal", (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
-});
-app.get("/api/keys/google", (req, res) => {
-  res.send({ key: process.env.GOOGLE_API_KEY || "" });
-});
+// app.get("/api/keys/paypal", (req, res) => {
+//   res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+// });
+// app.get("/api/keys/google", (req, res) => {
+//   res.send({ key: process.env.GOOGLE_API_KEY || "" });
+// });
 
 app.use("/api/upload", uploadRouter);
 app.use("/api/seed", seedRouter);
@@ -37,17 +35,19 @@ app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-);
+// Liên quan đến production
+// Chưa test nhưng đoán khả năng cao ở đây sai
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+  );
+}
 
-app.use((err, req, res, next) => {
-  res.status(500).send({ message: err.message });
-});
+// app.use((err, req, res, next) => {
+//   res.status(500).send({ message: err.message });
+// });
 
-const port = process.env.PORT || 4000;
-app.listen(port, () => {
-  console.log(`serve at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
