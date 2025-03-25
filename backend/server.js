affe6f226dbd69ade4236cfaf1da140d13a7767d
tmtuan04 <tuminhtuana7@gmail.com>
2025-03-25 11:37:43 +0700

    Fix bug Backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import path from "path";
 import dotenv from "dotenv";
-import seedRouter from "./routes/seedRoutes.js";
+// import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 // });
 
 app.use("/api/upload", uploadRouter);
-app.use("/api/seed", seedRouter);
+// app.use("/api/seed", seedRouter);
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
